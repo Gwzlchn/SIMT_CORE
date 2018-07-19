1f38b7b775ff99009b9e618972058bd5b980f536
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2018-07-19 11:28:17 +0800

    i-cache 读文件完成

---
+++ src/I_CACHE.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class I_CACHE {
+
+    private Instructions all_ins;
+    private JTable I_Cache_Table;
+    public I_CACHE(String file_name){
+        read_from_txt(file_name);
+
+    }
+    public void read_from_txt(String file_name){
+        all_ins = new Instructions(file_name);
+
+    }
+
+
+}
+
